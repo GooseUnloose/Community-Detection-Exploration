fd40b10b422f35d72e5374b13700766bd45034af
Goose <Gooseunloose@gmail.com>
2026-02-10 00:14:41 +0000

    fixed swapping of rows across dynamic tables

--- website/static/script.js
+++ website/static/script.js
@@ -31,14 +31,22 @@ function log_city_list (){
     document.getElementById('community_output').innerHTML = city_list;
 };
 
+
 function create_community_div(JSON_response){
     const keys = Object.keys(JSON_response);
 
-    const house_div = document.createElement('div');
+    const delete_button = document.createElement('button');
+    delete_button.className = 'community_delete';
+
+    const house_div = document.getElementById('test');
     keys.forEach(element => {
         const com_div = document.createElement('div');
         com_div.className = 'community_div';
         com_div.appendChild(create_response_table(JSON_response,element));
+
+        const delete_button = document.createElement('button');
+        delete_button.className = 'community_delete';
+        com_div.appendChild(delete_button);
 
         house_div.appendChild(com_div);
     });
@@ -48,8 +56,9 @@ function create_community_div(JSON_response){
 
 
 function drag_start_handler(ev){
-    ev.preventDefault();
-    ev.dataTransfer.setData('Text',ev.target.textContent);
+    //ev.preventDefault();
+    console.log(ev.target.textContent);
+    ev.dataTransfer.setData('Text',ev.target.id);
 }
 
 
@@ -61,34 +70,26 @@ function drag_over_handler(ev){
 function drop_handler(ev){
     ev.preventDefault();
 
-    console.log("Drop Complete");
+    console.log(ev.target.id);
 
     const drop_data = ev.dataTransfer.getData('Text')
-    const row = document.createElement('tr');
-    const row_data = document.createElement('td');
-
-    row_data.textContent = drop_data;
-
-    row.appendChild(row_data);
-
-    ev.target.appendChild(row);
-
-
+    ev.target.closest('Table').appendChild(document.getElementById(drop_data));
 }
-
 
 
 //Creates a new table for each key of the JSON response
 function create_response_table(JSON_response,key){
-    const com_table = document.createElement('table')
-
-    com_table.ondrop = 'drop_handler(event)';
+    const com_table = document.createElement('table');
+    com_table.id = String(key);
+    
 
     com_table.addEventListener('drop',function (e){
         drop_handler(e);
     })
 
-    com_table.ondragover = 'drag_over_handler(event)';
+    com_table.addEventListener('dragover', function (e){
+        drag_over_handler(e);
+    })
 
     const header_row = document.createElement('tr');
     const header = document.createElement('th');
@@ -100,18 +101,20 @@ function create_response_table(JSON_response,key){
 
     JSON_response[key].forEach(element => {
         const row = document.createElement('tr');
+        row.id = element;
+
         const row_data = document.createElement('td');
         row_data.textContent = element;
 
-        row_data.draggable = 'True';
-        
-        row_data.addEventListener('dragstart', function (e){
+        row.draggable = true;
+        row.addEventListener('dragstart', function (e) {
             drag_start_handler(e);
         })
 
-        row_data.addEventListener('dragover' , function (e){
-            drag_over_handler(e)
+        row.addEventListener('dragover', function (e){
+            drag_over_handler(e);
         })
+
 
         row.appendChild(row_data);
         com_table.appendChild(row);
@@ -135,13 +138,16 @@ document.getElementById('button').addEventListener('click',function (){
     request.onload = function() {
         return_response = JSON.parse(request.responseText);
 
-        const div_insert = document.getElementById('test');
-        div_insert.innerHTML = '';
-        div_insert.appendChild(create_community_div(return_response));
+        let div_insert = document.getElementById('test');
+        div_insert = create_community_div(return_response);
+        //div_insert.appendChild(create_community_div(return_response));
     }
 
     request.open('GET',`/louvain?cities_list=${city_list}`);
     request.send();
 })
 
-
+document.getElementsByClassName('community_delete').addEventListener('click', function (event){
+    const home_div = event.target.closest("div");
+    console.log(String(home_div));
+})
